f00dcbae9da8382bbc76b5f2165cef2a44ca65a3
Célian <celian.louis-itty@etu.univ-lyon1.fr>
2020-02-06 20:40:09 +0100

    Finished test for getPix

--- ModuleImage/Image.cpp
+++ ModuleImage/Image.cpp
@@ -55,51 +55,69 @@ void Image::erase(const Pixel& color)
 
 void Image::test()
 {
-	//Test of getPix
-	Pixel p = Pixel(255, 255, 255); 
-	getPix(3, 4) = p;
-	Pixel check = tab[4 * dimx + 3];
-	int red = check.getRed();
-	cout << red << endl;
+//Test of Pixel equality
+	Pixel p1(255, 255, 255);
+	Pixel p2(255, 255, 255);
+	if (p1 == p2)
+	{
+		cout << "Pixel Equality check is valid!" << endl << endl;
+
+		//Test of getPix
+
+		bool isGetPixValid = true;
+		Pixel& pix = getPix(3, 4);
+		Pixel checkBefore = tab[4 * dimx + 3];
+		if (!(checkBefore == pix))
+		{
+			cout << "ERROR : Pixel value not retured from getPix" << endl << endl;
+			isGetPixValid = false;
+		}
+		Pixel checkAfter(255, 255, 255);
+		pix = checkAfter;
+		if (!(checkAfter == tab[4 * dimx + 3]))
+		{
+			cout << "ERROR : Pixel retured from getPix is not the original (not a reference)" << endl << endl;
+			isGetPixValid = false;
+		}
+		if (isGetPixValid) cout << "getPix is Valid!!" << endl << endl;
 
 
-	int a = 1;
-	int &b = a;
+		// Test rect
 
+		Pixel couleur;
+		drawRect(15, 20, 50, 75, couleur);
+		for (int i = 15; i < 50; i++) {
+			for (int j = 20; j < 75; j++) {
 
-// Test rect
-
-    Pixel couleur;
-	drawRect(15,20,50,75,couleur);
-	for(int i=15;i<50;i++){
-        for(int j=20;j<75;i++){
-
-        }
+			}
+		}
+		//Test setPix
+		Pixel pInit;
+		Pixel pRef;
+		pInit.setBlue(0);
+		pInit.setGreen(0);
+		pInit.setRed(0);
+		setPix(1, 1, pInit);
+		pRef.setBlue(0);
+		pRef.setGreen(0);
+		pRef.setRed(0);
+		if (pRef.getBlue() == pInit.getBlue())
+		{
+			if (pRef.getGreen() == pInit.getGreen())
+			{
+				if (pRef.getRed() == pInit.getRed())
+				{
+					cout << "setPix est validee" << endl << endl;
+				}
+			}
+		}
+		else
+		{
+			cout << "setPix ne marche pas" << endl << endl;
+		}
 	}
-
-
-//Test setPix
-	Pixel pInit;
-	Pixel pRef;
-	pInit.setBlue(0);
-	pInit.setGreen(0);
-	pInit.setRed(0);
-	setPix(1, 1, pInit);
-	pRef.setBlue(0);
-	pRef.setGreen(0);
-	pRef.setRed(0);
-    if(pRef.getBlue()==pInit.getBlue())
-    {
-        if(pRef.getGreen()==pInit.getGreen())
-        {
-            if(pRef.getRed()==pInit.getRed())
-            {
-                cout << "setPix est validée";
-            }
-        }
-    }
-    else
-    {
-        cout << "setPix ne marche pas";
-    }
+	else // Pixel equality check failed...
+	{
+		cout << "ERROR : The Pixel equality check is not fonctional. The other tests therefore couldn't be completed" << endl;
+	}
 }
